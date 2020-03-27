feea02e2d59aa2876b0b4252947e898a1d13dbff
Nicholas Murray <nickmurray91@gmail.com>
2020-03-28 00:19:36 +1100

    merged master into branch

---
+++ .dart_tool/flutter_build/4ebf87b79fd5ec5c67e3352c4b6e1071/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:planning_poker/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
