703b496df3a087d3450f2655b55c8d3229b88165
Nicholas Murray <nickmurray91@gmail.com>
2020-03-29 01:56:15 +1100

    implemented a rough estimation implementation

--- lib/main.dart
+++ lib/main.dart
@@ -28,35 +28,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final myController = TextEditingController();
+  
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: GestureDetector(
           onTap: () {
-            Firestore.instance.collection('estimates').document('Nick').setData({'estimate': 8});
+            Firestore.instance.collection('estimates').document('Gary').setData({'estimate': 3});
           },
           child: Text(widget.title)
         ),
       ),
-      body: Center(
-        child: StreamBuilder(
-          stream: Firestore.instance.collection('estimates').snapshots(),
-          builder: (context, snapshot)
-          {
-            if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting: 
-                return new Text('Loading...');
-              default:
-                return new Wrap(
-                  children: snapshot.data.documents.map<Widget>((DocumentSnapshot document) {
-                    return PokerCard(document: document);
-                  }).toList()
-                );
-            }
-          }
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: myController,
+                  style: TextStyle(fontSize: 30),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                  ),
+                ),
+              ),
+              StreamBuilder(
+                stream: Firestore.instance.collection('estimates').snapshots(),
+                builder: (context, snapshot)
+                {
+                  if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting: 
+                      return new Text('Loading...');
+                    default:
+                      return new Wrap(
+                        alignment: WrapAlignment.center,
+                        children: snapshot.data.documents.map<Widget>((DocumentSnapshot document) {
+                          return PokerCard(document: document);
+                        }).toList()
+                      );
+                  }
+                }
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('1'),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Firestore.instance
+                      .collection('estimates')
+                      .document(myController.text)
+                      .setData({'estimate': 1});
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('2'),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Firestore.instance
+                      .collection('estimates')
+                      .document(myController.text)
+                      .setData({'estimate': 2});
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('3'),
+                    color: Colors.blue,
+                    onPressed: () {
+                      Firestore.instance
+                      .collection('estimates')
+                      .document(myController.text)
+                      .setData({'estimate': 3});
+                    },
+                  ),
+                ],
+              )
+            ]
+          ),
+        ],
       )
     );
   }
