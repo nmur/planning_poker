0eb54f922a6ceee7c631fe5411a8833a9c8280ff
Nicholas Murray <nickmurray91@gmail.com>
2020-03-31 01:13:02 +1100

    introduced "revelead" doc property, and styling for it

--- lib/poker_card.dart
+++ lib/poker_card.dart
@@ -15,26 +15,29 @@ class PokerCard extends StatelessWidget {
       margin: EdgeInsets.all(20),
       elevation: 5,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width * 0.009)),
-        side: new BorderSide(color: Colors.blue, width: MediaQuery.of(context).size.width * 0.007),
+        borderRadius: BorderRadius.all(
+            Radius.circular(MediaQuery.of(context).size.width * 0.009)),
+        side: new BorderSide(
+            color: document['revealed'] ? Colors.blue : Colors.grey,
+            width: MediaQuery.of(context).size.width * 0.007),
       ),
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.09,
         height: MediaQuery.of(context).size.width * 0.15,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.width * 0.02),
-            new Text(
-              document['estimate'].toString(),
-              style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06),
-            ),
-            new Text(
-              document.documentID,
-              style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.01),
-            )
-          ]
-        ),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          SizedBox(height: MediaQuery.of(context).size.width * 0.02),
+          new Text(
+            document['revealed'] ? document['estimate'].toString() : '?',
+            style:
+                TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06),
+          ),
+          new Text(
+            document.documentID,
+            style:
+                TextStyle(fontSize: MediaQuery.of(context).size.width * 0.01),
+          )
+        ]),
       ),
     );
   }
