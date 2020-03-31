9ef4652f1ea53638931855fd27addcfba6089468
Nicholas Murray <nickmurray91@gmail.com>
2020-04-01 01:38:08 +1100

    replaced all estimation/delete buttons with lil cards

--- lib/estimation_button_bar.dart
+++ lib/estimation_button_bar.dart
@@ -12,6 +12,7 @@ class EstimationButtonBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ButtonBar(
+      buttonPadding: EdgeInsets.all(0),
       alignment: MainAxisAlignment.center,
       children: <Widget>[
         buildEstimationButton(value: 0),
@@ -23,42 +24,55 @@ class EstimationButtonBar extends StatelessWidget {
         buildEstimationButton(value: 13),
         buildEstimationButton(value: 20),
         buildEstimationButton(value: 40),
-        FlatButton(
-          child: Icon(
-            Icons.delete,
-            color: Colors.white,
-            size: 24.0,
+        GestureDetector(
+          child: new Card(
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(7)),
+              side: new BorderSide(color: Colors.red, width: 5),
+            ),
+            child: SizedBox(
+                width: 60,
+                height: 100,
+                child: Center(
+                    child: Icon(
+                  Icons.delete,
+                  size: 40.0,
+                ))),
           ),
-          color: Colors.red,
-          onPressed: () {
+          onTap: () {
             Firestore.instance
                 .collection('estimates')
                 .document(myController.text)
                 .delete();
           },
         ),
-        FlatButton(
-            child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                  size: 24.0,
-                ),
-                Text('ALL'),
-              ],
+        GestureDetector(
+          child: new Card(
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(7)),
+              side: new BorderSide(color: Colors.red, width: 5),
             ),
-            color: Colors.red,
-            onPressed: () {
-              Firestore.instance
-                  .collection('estimates')
-                  .getDocuments()
-                  .then((snapshot) {
-                for (DocumentSnapshot documentSnapshot in snapshot.documents) {
-                  documentSnapshot.reference.delete();
-                }
-              });
-            }),
+            child: SizedBox(
+                width: 60,
+                height: 100,
+                child: Column(children: <Widget>[
+                  SizedBox(height: 30),
+                  Icon(
+                    Icons.delete,
+                    size: 40.0,
+                  ),
+                  Text('ALL', style: TextStyle(fontSize: 18),),
+                ])),
+          ),
+          onTap: () {
+            Firestore.instance
+                .collection('estimates')
+                .document(myController.text)
+                .delete();
+          },
+        ),
       ],
     );
   }
@@ -68,21 +82,17 @@ class EstimationButtonBar extends StatelessWidget {
       child: new Card(
         elevation: 3,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(3)),
-          side: new BorderSide(
-              color: Colors.blue,
-              width: 5),
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          side: new BorderSide(color: Colors.blue, width: 5),
         ),
         child: SizedBox(
-          width: 60,
-          height: 100,
-          child: Center(
-            child: Text(
+            width: 60,
+            height: 100,
+            child: Center(
+                child: Text(
               value.toString(),
               style: TextStyle(fontSize: 40),
-            )
-          )
-        ),
+            ))),
       ),
       onTap: () {
         Firestore.instance
